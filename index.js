33c67b2eb163d91b0f139369b4362430f806b161
dugon <duongsn231998@gmail.com>
2019-01-17 19:17:53 +0700

    sefl learn expressjs

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 3000
 
-app.get('/',(req,res) => res.send('Hello World!'))
-app.listen(port,() => console.log('listening on port 8080'))
+app.set('view engine', 'pug')
+app.set('views', './views')
+app.get('/',(req,res) => res.render('index',{name:"duong"}))
+app.get('/student',(req,res) => res.render('student',{
+    students:[
+        {name:"duong"},
+        {name:"bac"}
+    ]
+}))
+app.listen(port,() => console.log('listening on port'+port))
